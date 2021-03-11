b37308b3247f86d5db805ef761619225514c9f58
GustafsonSK <76776543+GustafsonSK@users.noreply.github.com>
2021-03-11 08:58:27 +0100

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -17,8 +17,6 @@ int main()
 {
 	Poloha normal;
 	movement Move;
-	float InputX = 0;
-	float InputZ = 1;	
 	
 	wiringPiSetup(); // Aktivacia GPIO
 	
@@ -27,7 +25,6 @@ int main()
 		
 	int servo2 = pca9685Setup(400, 0x41, 50);
 		pca9685PWMReset(servo2);
-	pwmWrite(304,380);
 
 /// Nohy	
 	normal.x = 1;
@@ -58,11 +55,10 @@ int main()
 	for(int i = 0; i<10;i++)
 	{
 		cout<<i<<endl;
-		delay(500);
+		delay(100);
 	}
 	while (true)
 	{
-		
 		Move.normal();
 		Noha2.pohyb(Move.Ciel2);
 		Noha3.pohyb(Move.Ciel3);
@@ -75,166 +71,64 @@ int main()
 			pwmWrite(300+Noha1.pin,map(Noha1.uhol1,500,120));
 			pwmWrite(300+Noha1.pin+1,map(Noha1.uhol2,500,120));
 			pwmWrite(300+Noha1.pin+2,map(Noha1.uhol3,500,120));
-			delay(10);
+			delay(100);
+			cout<<"Noha1: "<<Noha1.uhol1<<" "<<Noha1.uhol2<<" "<<Noha1.uhol3<<endl;
 			pwmWrite(300+Noha3.pin,map(Noha3.uhol1,500,120));
 			pwmWrite(300+Noha3.pin+1,map(Noha3.uhol2,500,120));
 			pwmWrite(300+Noha3.pin+2,map(Noha3.uhol3,500,120));
-			delay(10);
+			delay(100);
+			cout<<"Noha2: "<<Noha2.uhol1<<" "<<Noha2.uhol2<<" "<<Noha2.uhol3<<endl;
 			pwmWrite(300+Noha5.pin,map(Noha5.uhol1,500,120));
-			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120));
+			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120)+20);
 			pwmWrite(300+Noha5.pin+2,map(Noha5.uhol3,500,120));
-			delay(10);
+			delay(100);
+			cout<<"Noha3: "<<Noha3.uhol1<<" "<<Noha3.uhol2<<" "<<Noha3.uhol3<<endl;
 			pwmWrite(400+Noha2.pin,map(Noha2.uhol1,120,500));
 			pwmWrite(400+Noha2.pin+1,map(Noha2.uhol2,120,500));
 			pwmWrite(400+Noha2.pin+2,map(Noha2.uhol3,120,500));
-			delay(10);
+			delay(100);
+			cout<<"Noha4: "<<Noha4.uhol1<<" "<<Noha4.uhol2<<" "<<Noha4.uhol3<<endl;
 			pwmWrite(400+Noha4.pin,map(Noha4.uhol1,120,500));
 			pwmWrite(400+Noha4.pin+1,map(Noha4.uhol2,120,500));
 			pwmWrite(400+Noha4.pin+2,map(Noha4.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha6.pin,map(Noha6.uhol1,120,500));
+			delay(100);
+			cout<<"Noha5: "<<Noha5.uhol1<<" "<<Noha5.uhol2<<" "<<Noha5.uhol3<<endl;
+			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol1,120,500));
 			pwmWrite(400+Noha6.pin+1,map(Noha6.uhol2,120,500));
-			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol3,120,500));
-			
-			cout<<"normal"<<endl;
+			pwmWrite(400+Noha6.pin,map(Noha6.uhol3,120,500));
+			delay(100);
+			cout<<"Noha6: "<<Noha6.uhol1<<" "<<Noha6.uhol2<<" "<<Noha6.uhol3<<endl;
+			cout<<"normal\n";
 			delay(500);
 			
-		Move.move1(InputX, InputZ);
-			Noha2.pohyb(Move.Ciel2);
-			Noha3.pohyb(Move.Ciel3);
-			Noha6.pohyb(Move.Ciel6);
-			Noha1.pohyb(Move.Ciel1);
-			Noha4.pohyb(Move.Ciel4);
-			Noha5.pohyb(Move.Ciel5);
-			pwmWrite(300+Noha1.pin,map(Noha1.uhol1,500,120));
-			pwmWrite(300+Noha1.pin+1,map(Noha1.uhol2,500,120));
-			pwmWrite(300+Noha1.pin+2,map(Noha1.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha3.pin,map(Noha3.uhol1,500,120));
-			pwmWrite(300+Noha3.pin+1,map(Noha3.uhol2,500,120));
-			pwmWrite(300+Noha3.pin+2,map(Noha3.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha5.pin,map(Noha5.uhol1,500,120));
-			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120));
-			pwmWrite(300+Noha5.pin+2,map(Noha5.uhol3,500,120));
-			delay(10);
-			pwmWrite(400+Noha2.pin,map(Noha2.uhol1,120,500));
-			pwmWrite(400+Noha2.pin+1,map(Noha2.uhol2,120,500));
-			pwmWrite(400+Noha2.pin+2,map(Noha2.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha4.pin,map(Noha4.uhol1,120,500));
-			pwmWrite(400+Noha4.pin+1,map(Noha4.uhol2,120,500));
-			pwmWrite(400+Noha4.pin+2,map(Noha4.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha6.pin,map(Noha6.uhol1,120,500));
-			pwmWrite(400+Noha6.pin+1,map(Noha6.uhol2,120,500));
-			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol3,120,500));
-			cout<<"move1"<<endl;
-			delay(500);
-	
-		Move.dole();
-			Noha2.pohyb(Move.Ciel2);
-			Noha3.pohyb(Move.Ciel3);
-			Noha6.pohyb(Move.Ciel6);
-			pwmWrite(300+Noha1.pin,map(Noha1.uhol1,500,120));
-			pwmWrite(300+Noha1.pin+1,map(Noha1.uhol2,500,120));
-			pwmWrite(300+Noha1.pin+2,map(Noha1.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha3.pin,map(Noha3.uhol1,500,120));
-			pwmWrite(300+Noha3.pin+1,map(Noha3.uhol2,500,120));
-			pwmWrite(300+Noha3.pin+2,map(Noha3.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha5.pin,map(Noha5.uhol1,500,120));
-			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120));
-			pwmWrite(300+Noha5.pin+2,map(Noha5.uhol3,500,120));
-			delay(10);
-			pwmWrite(400+Noha2.pin,map(Noha2.uhol1,120,500));
-			pwmWrite(400+Noha2.pin+1,map(Noha2.uhol2,120,500));
-			pwmWrite(400+Noha2.pin+2,map(Noha2.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha4.pin,map(Noha4.uhol1,120,500));
-			pwmWrite(400+Noha4.pin+1,map(Noha4.uhol2,120,500));
-			pwmWrite(400+Noha4.pin+2,map(Noha4.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha6.pin,map(Noha6.uhol1,120,500));
-			pwmWrite(400+Noha6.pin+1,map(Noha6.uhol2,120,500));
-			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol3,120,500));
-			cout<<"dole"<<endl;
-			delay(500);
+		Move.doleI -=10;
+		cout<<"Dole: "<<Move.doleI<<endl;
 			
 			
-		Move.move2(InputX, InputZ);
-		Move.move2(InputX, InputZ);
-			Noha1.pohyb(Move.Ciel1);
-			Noha4.pohyb(Move.Ciel4);
-			Noha5.pohyb(Move.Ciel5);
-			Noha2.pohyb(Move.Ciel2);
-			Noha3.pohyb(Move.Ciel3);
-			Noha6.pohyb(Move.Ciel6);
-
-			pwmWrite(300+Noha1.pin,map(Noha1.uhol1,500,120));
-			pwmWrite(300+Noha1.pin+1,map(Noha1.uhol2,500,120));
-			pwmWrite(300+Noha1.pin+2,map(Noha1.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha3.pin,map(Noha3.uhol1,500,120));
-			pwmWrite(300+Noha3.pin+1,map(Noha3.uhol2,500,120));
-			pwmWrite(300+Noha3.pin+2,map(Noha3.uhol3,500,120));
-			delay(10);
-			pwmWrite(300+Noha5.pin,map(Noha5.uhol1,500,120));
-			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120));
-			pwmWrite(300+Noha5.pin+2,map(Noha5.uhol3,500,120));
-			delay(10);
-			pwmWrite(400+Noha2.pin,map(Noha2.uhol1,120,500));
-			pwmWrite(400+Noha2.pin+1,map(Noha2.uhol2,120,500));
-			pwmWrite(400+Noha2.pin+2,map(Noha2.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha4.pin,map(Noha4.uhol1,120,500));
-			pwmWrite(400+Noha4.pin+1,map(Noha4.uhol2,120,500));
-			pwmWrite(400+Noha4.pin+2,map(Noha4.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha6.pin,map(Noha6.uhol1,120,500));
-			pwmWrite(400+Noha6.pin+1,map(Noha6.uhol2,120,500));
-			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol3,120,500));
-			cout<<"move2"<<endl;
-			delay(500);
-			
-		Move.dole();
-			Noha1.pohyb(Move.Ciel1);
-			Noha4.pohyb(Move.Ciel4);
-			Noha5.pohyb(Move.Ciel5);
-		Move.normal();
+		/*Move.move1(0,1);
 		Noha2.pohyb(Move.Ciel2);
 		Noha3.pohyb(Move.Ciel3);
 		Noha6.pohyb(Move.Ciel6);
 		Noha1.pohyb(Move.Ciel1);
 		Noha4.pohyb(Move.Ciel4);
 		Noha5.pohyb(Move.Ciel5);
+		
+		
 			pwmWrite(300+Noha1.pin,map(Noha1.uhol1,500,120));
 			pwmWrite(300+Noha1.pin+1,map(Noha1.uhol2,500,120));
 			pwmWrite(300+Noha1.pin+2,map(Noha1.uhol3,500,120));
-			delay(10);
+			delay(1000);
 			pwmWrite(300+Noha3.pin,map(Noha3.uhol1,500,120));
 			pwmWrite(300+Noha3.pin+1,map(Noha3.uhol2,500,120));
 			pwmWrite(300+Noha3.pin+2,map(Noha3.uhol3,500,120));
-			delay(10);
+			delay(1000);
 			pwmWrite(300+Noha5.pin,map(Noha5.uhol1,500,120));
 			pwmWrite(300+Noha5.pin+1,map(Noha5.uhol2,500,120));
 			pwmWrite(300+Noha5.pin+2,map(Noha5.uhol3,500,120));
-			delay(10);
-			pwmWrite(400+Noha2.pin,map(Noha2.uhol1,120,500));
-			pwmWrite(400+Noha2.pin+1,map(Noha2.uhol2,120,500));
-			pwmWrite(400+Noha2.pin+2,map(Noha2.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha4.pin,map(Noha4.uhol1,120,500));
-			pwmWrite(400+Noha4.pin+1,map(Noha4.uhol2,120,500));
-			pwmWrite(400+Noha4.pin+2,map(Noha4.uhol3,120,500));
-			delay(10);
-			pwmWrite(400+Noha6.pin,map(Noha6.uhol1,120,500));
-			pwmWrite(400+Noha6.pin+1,map(Noha6.uhol2,120,500));
-			pwmWrite(400+Noha6.pin+2,map(Noha6.uhol3,120,500));
-			cout<<"servo normal"<<endl;
-			delay(500);
-		
+			cout<<"move1\n";
+			delay(3000);*/
+			
+			
 	}
 	
 
